425ba8310022d7eace7c6bb633712193ebe53b0d
Matheus Battistella <theus.baptistella@gmail.com>
2025-10-01 20:39:14 -0300

    ajustar a configuração da base no Vite para 'gestao_brab'

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/brab-site/',
+  base: '/gestao_brab/',
   plugins: [react()]
    
 })
